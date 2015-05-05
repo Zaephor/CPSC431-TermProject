b5b1a8617c89701307fcdfab2451c8991566cc2d
Zaephor <github@draconrose.com>
2015-05-04 18:11:19 -0700

    Added a user controller for login/logout. Also wrapped all existing routes into /api/ like in our presentation

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zaephor
+ * Date: 5/4/15
+ * Time: 6:08 PM
+ */
+
+namespace app\Http\Controllers;
+
+
+class UserController extends Controller {
+    public function postLogin(){
+        return array("Login");
+    }
+    public function postLogout(){
+        return array("Logout");
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,43 +16,46 @@ Route::get('/', 'WelcomeController@index');
 //Route::get('home', 'HomeController@index');
 
 Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
+    'auth' => 'Auth\AuthController',
+    'password' => 'Auth\PasswordController',
 ]);
 
-Route::post('login','TestController@debug');
-Route::post('logout','TestController@debug');
 
-Route::group(['prefix'=>'student'],function(){
-    Route::get('grades','StudentController@getGrades');
-    Route::get('courses','StudentController@getCourses');
-    Route::post('enroll/{session_id}','TestController@debug');
-    Route::group(['prefix'=>'course'],function(){
-        Route::get('{session_id}','TestController@debug');
-        Route::put('{session_id}/upload','TestController@debug');
-    });
-});
+Route::group(['prefix' => 'api'], function () {
+    Route::post('login', 'UserController@postLogin');
+    Route::post('logout', 'UserController@postLogout');
 
-Route::group(['prefix'=>'faculty'],function(){
-    Route::get('course/{course_id}','FacultyController@getCourse');
-    Route::get('grades/{student_id}','FacultyController@getGrades');
-    Route::get('sessions','FacultyController@getSessions');
-    Route::group(['prefix'=>'session'],function(){
-        Route::get('{session_id}/assignments','TestController@debug');
-        Route::put('{session_id}/upload','TestController@debug');
+    Route::group(['prefix' => 'student'], function () {
+        Route::get('grades', 'StudentController@getGrades');
+        Route::get('courses', 'StudentController@getCourses');
+        Route::post('enroll/{session_id}', 'TestController@debug');
+        Route::group(['prefix' => 'course'], function () {
+            Route::get('{session_id}', 'TestController@debug');
+            Route::put('{session_id}/upload', 'TestController@debug');
+        });
     });
-});
 
-Route::group(['prefix'=>'admin'],function(){
-    Route::get('courses','AdminController@getCourses');
-    Route::group(['prefix'=>'course/{course_id}'],function(){
-        Route::get('/','AdminController@getCourseInfo');
-        Route::post('modify','TestController@debug');
-        Route::put('add','TestController@debug');
-        Route::delete('delete','TestController@debug');
+    Route::group(['prefix' => 'faculty'], function () {
+        Route::get('course/{course_id}', 'FacultyController@getCourse');
+        Route::get('grades/{student_id}', 'FacultyController@getGrades');
+        Route::get('sessions', 'FacultyController@getSessions');
+        Route::group(['prefix' => 'session'], function () {
+            Route::get('{session_id}/assignments', 'TestController@debug');
+            Route::put('{session_id}/upload', 'TestController@debug');
+        });
     });
-    Route::group(['prefix'=>'session/{session_id}'],function(){
-        Route::put('add','TestController@debug');
-        Route::delete('delete','TestController@debug');
+
+    Route::group(['prefix' => 'admin'], function () {
+        Route::get('courses', 'AdminController@getCourses');
+        Route::group(['prefix' => 'course/{course_id}'], function () {
+            Route::get('/', 'AdminController@getCourseInfo');
+            Route::post('modify', 'TestController@debug');
+            Route::put('add', 'TestController@debug');
+            Route::delete('delete', 'TestController@debug');
+        });
+        Route::group(['prefix' => 'session/{session_id}'], function () {
+            Route::put('add', 'TestController@debug');
+            Route::delete('delete', 'TestController@debug');
+        });
     });
 });
