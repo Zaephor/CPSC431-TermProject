bdaf54f35909f3acd951b357d64d5d6622c32a59
Zaephor <github@draconrose.com>
2015-05-07 11:47:15 -0700

    Added Cross site origin support

---
+++ app/Http/Middleware/CORS.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zaephor
+ * Date: 5/7/15
+ * Time: 11:44 AM
+ */
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+use Illuminate\Contracts\Routing\Middleware;
+use Illuminate\Http\Response;
+
+class CORS implements Middleware {
+    public function handle($request, Closure $next) {
+        return $next($request)->header('Access-Control-Allow-Origin' , '*')
+            ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With');
+    }
+}
