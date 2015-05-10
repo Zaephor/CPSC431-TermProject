f84dcf2ca3b8ffdab07e5082f48007f48fe11265
Zaephor <github@draconrose.com>
2015-05-09 20:23:49 -0700

    Testing PDF

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -134,8 +134,8 @@ class StudentController extends Controller
             $session->load('course', 'course.department', 'professor');
         }
 //        return array('status' => $status, 'data' => $session);
-        $pdf = PDF::loadView('pdf.invoice', $session);
-        return $pdf->download('invoice.pdf');
+        $pdf = PDF::loadView('pdf.syllabus', $session);
+        return $pdf->download('syllabus.pdf');
     }
 
     public function getAssignments()
---
+++ resources/views/pdf.syllabus.blade.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+<p>
+    {{ $session }}
+</p>
+</body>
+</html>
