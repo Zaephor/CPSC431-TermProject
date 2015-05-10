89408b5bab639424b6d4479eec0c626ca0e40dc9
Zaephor <github@draconrose.com>
2015-05-09 20:58:23 -0700

    Testing PDF

--- resources/views/syllabus.blade.php
+++ resources/views/syllabus.blade.php
@@ -1,7 +1,16 @@
 <html>
 <body>
 <p>
-    {{ print_r($session) }}
+    {{ $session->course->department->title }}<br/>
+    {{ $session->course->department->code }}{{ $session->course->code }}<br/>
+    Session {{ $session->id }}<br/>
+    {{ $session->course->professor->name }}<br/>
+    {{ $session->course->professor->email }}<br/>
+    {{ $session->room }}<br/>
+</p>
+
+<p>
+    {{ 'Hello?' }}
 </p>
 </body>
 </html>
