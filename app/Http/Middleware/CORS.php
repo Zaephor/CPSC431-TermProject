ed03aa66e30de940dbe040552d88ce5773f712db
Zaephor <github@draconrose.com>
2015-05-11 13:50:59 -0700

    Testing something

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -16,7 +16,7 @@ use Illuminate\Http\Response;
 class CORS implements Middleware {
     public function handle($request, Closure $next) {
         return $next($request)->header('Access-Control-Allow-Origin' , '*')
-            ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
+            ->header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With');
     }
 }
