c39a45ad492b322d499ac3357b01e61deaf22793
Zaephor <github@draconrose.com>
2015-05-11 22:26:39 -0700

    Fixes and code cleanup

--- app/Http/Controllers/FacultyController.php
+++ app/Http/Controllers/FacultyController.php
@@ -86,12 +86,9 @@ class FacultyController extends Controller
         $course = Course::with(['sessions' => function ($query) use ($userAuth) {
             $query->where('professor_id', '=', $userAuth->id)->with('professor');
         },'department'])->get();
-//        $session = Session::where('professor_id','=',$user->id)->get();
         $status = 404;
         if (sizeof($course) > 0) {
             $status = 200;
-//            $session->load('course', 'course.department', 'professor', 'assignments', 'students');
-//            $course->load('department','professor');
         }
         return array('status' => $status, 'data' => $course);
     }
